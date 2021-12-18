218dc5e55bab6f6b3aca8fd716552331ae56b667
Julian <julian.kath@posteo.de>
2021-12-18 21:04:30 +0100

    update dash

--- src/Module 3 dash.py
+++ src/Module 3 dash.py
@@ -30,7 +30,7 @@ app.layout = html.Div(
             dcc.Tab(label="Text Classification", value='tab-4'),
         ]),
         html.Div(id='tab-content')
-    ])
+    ], style={'margin':'auto', 'padding': '30px', 'align': 'center'})
 
 @app.callback(Output('tab-content', 'children'), Input('tabs', 'value'))
 def render_content(tab):
@@ -39,8 +39,10 @@ def render_content(tab):
             html.H3('Overview'),
             html.Div(children=[
                 html.H1("Project Title"),
-                html.H2("Description, blabla")
-            ], style={'height':250, 'width':250, 'background-color':'cornflowerblue'})
+                html.H5("""Every person that is located in the European Union’s territory has and is owed human rights, which they can assert against the State, companies, the police, and your fellow Hertie lecturers or class mates. However, people are unaware of what their rights are, how rights evolve, and what rights even mean. The HUDOC European Union Court of Human Rights database provides access to all the relevant European Union human rights case law. However, this database fails at making this law accessible to the people it serves."""),
+                html.H5("""Magna Carta Mining fills the gap left by HUDOC and makes the legal information accessible to the European Union’s population by providing the relevant information in a manner that is readily understandable (data visualisation), providing the user with the the ability to identify trends in human rights issues, and with the ability to download documents en masse. The data visualisations pertain to rights based issues that are generalised or specific to the user."""),
+                html.H5("""Specific information can be selected by the user who will be provided with a multitude of infographics which they can manipulate. Examples of human rights information that the user can identify include, but is not limited to: the change in human rights issues over time; the number of cases decided by European Member States in any given year; or the most topical issue for any or all European Member States.""")
+            ], style={'height':400, 'width':800, 'background-color':'cornflowerblue', 'margin': 'auto', 'font-family': 'Sans-serif', 'padding': '10px', 'color': 'white'})
         ])
     elif tab == 'tab-2':
         return html.Div([
@@ -48,7 +50,7 @@ def render_content(tab):
             dcc.Graph(
                 id='line_plot',
                 figure=line_plot,
-                style={'width': '100vh', 'height': '90vh'}
+                style={'width': '100vh', 'height': '90vh', 'margin': 'auto'}
             )
         ])
     elif tab == 'tab-3':
@@ -57,7 +59,7 @@ def render_content(tab):
             dcc.Graph(
                id='network_plot',
                figure=network_plot,
-               style={'width': '100vh', 'height': '90vh'} 
+               style={'width': '100vh', 'height': '90vh', 'margin': 'auto'} 
             )
         ])
     elif tab == 'tab-4':
@@ -66,7 +68,7 @@ def render_content(tab):
             dcc.Graph(
                id='cm_plot',
                figure=cm_plot,
-               style={'width': '100vh', 'height': '90vh'}
+               style={'width': '100vh', 'height': '90vh', 'margin': 'auto'}
             )
         ])
 
