bfcae7265ccfe63a42d1696d5ff5dfd985495ea0
Julian <julian.kath@posteo.de>
2021-12-19 20:29:14 +0100

    updates

--- src/Module 4 dash.py
+++ src/Module 4 dash.py
@@ -26,9 +26,10 @@ articles_plot = pio.read_json('output/plotly_sb_art.json')
 echr_logo = 'https://www.mediadefence.org/wp-content/uploads/2020/06/Logo_European_Court_of_Human_Rights_1_linedrawing.jpg'
 github_logo = 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'
 
-# links to github & hudoc
+# links to github, hudoc & spacy
 hudoc_link = 'https://hudoc.echr.coe.int/eng#{%22documentcollectionid2%22:[%22GRANDCHAMBER%22,%22CHAMBER%22]}'
 github_link = 'https://github.com/juka19/Magna-Carta-Mining'
+spacy_link = "https://spacy.io/api/architectures#TextCatBOW"
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.LUMEN],
     meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale= 1.0'}]) # importing bootstrap css theme Lumen
@@ -78,11 +79,23 @@ def render_content(tab):
         return html.Div([
             html.H3('Overview'),
             html.Div(children=[
-                html.H1("Magna Carta Mining"),
-                html.P("""Every person that is located in the European Union’s territory has and is owed human rights, which they can assert against the State, companies, the police, and your fellow Hertie lecturers or class mates. However, people are unaware of what their rights are, how rights evolve, and what rights even mean. The HUDOC European Union Court of Human Rights database provides access to all the relevant European Union human rights case law. However, this database fails at making this law accessible to the people it serves."""),
-                html.P("""Magna Carta Mining fills the gap left by HUDOC and makes the legal information accessible to the European Union’s population by providing the relevant information in a manner that is readily understandable (data visualisation), providing the user with the the ability to identify trends in human rights issues, and with the ability to download documents en masse. The data visualisations pertain to rights based issues that are generalised or specific to the user."""),
-                html.P("""Specific information can be selected by the user who will be provided with a multitude of infographics which they can manipulate. Examples of human rights information that the user can identify include, but is not limited to: the change in human rights issues over time; the number of cases decided by European Member States in any given year; or the most topical issue for any or all European Member States.""")
-            ], style={'height': '50%', 'width':'80%', 
+                html.H2("Magna Carta Mining"),
+                html.P("""Every person located in the territorial jurisdiction of the European Court of Human Rights is owed human rights by the respective member state. People can use these rights against the State and State organs. However, people are unaware of what their rights are, how their rights evolve, and what their rights even mean. The HUDOC European Court of Human Rights database provides access to all relevant European Union human rights case law, but fails to make the underlying information accessible to the people it serves."""),
+                html.P("""Magna Carta mining provides easy to use infographics that users can drill down to obtain the relevant data they might require. This tool can be used by individuals, Governments, researchers , and politicians to obtain a high level and detailed overview of human rights in the European Union. Currently, users can view the following infographics: Judgements over Time; Network Graph; Text Classification; and Allegations by Country, Article & Judge."""),
+                html.H3('Judgements over Time'),
+                html.P("""Judgments over time, for every member state, details the number of judgements issues in the year. This graph instantly provides insight to what Member States are being litigated against most frequently and relatively to other Member States. For example, preliminary insight would show that between 1995 and 2000, Turkey was the most litigated against state. However, this changed at 2005 where Italy became the most litigated against state. Insight such as this is not instantly obtainable using the HUDOC database."""),
+                html.P("""Possibly unsurprisingly, the number of cases decreased during 2020, this may be correlated to the COVID-19 pandemic. A future infographic could include articles alleged over time, to understand whether there has been a shift in what the most important rights at the time are."""),
+                html.H3("Network Graph"),
+                html.P("""The Network Graph displays which cases are linked to other cases. The cases on the Network Graph must be linked to at least 5 other cases to be displayed. In summary, this graph displays what the most commonly cited cases are alternatively what cases have the highest precedent value. If a researcher, lawyer, or student wishes to identify the most important cases in a given area quickly, they can click through this infographic instead of conducting primary research and reading large amounts of material."""),
+                html.P("""At a conceptual level, this graph also shows the links between all areas of law. If someone wishes how a breach of one human rights impacts another, the user could simply trace through this Network Graph. A further development would be to expand the network graph to the lower Chamber and make it searchable with callbacks."""),
+                html.H3("Text Classification"),
+                html.P("""We trained a spacy Bag-of-Words model on the texts of the judgements, using a constructed label from the metadata of the judgement that yields four categories: No violation, Violation, Other (violation is not mentioned in the judgements conclusion) and Mixed (which indicates that there is both violations and non-violations present in the conlusion)."""),
+                html.P("""As expected, the model is far from being perfect! It overestimates the labels Violation and Other, and almost never predicts Non-violation and Mixed (which is probably very difficult!). You can test the model's predictions and see how it scores your text. We are also reporting the confusion matrix below the model that plots the predicted labels against the true labels from our test dataset."""),
+                html.A(html.P('See here for the model architecture.'), href=spacy_link, className='link-light text-light'),
+                html.H3("""Sunburst Plots"""),
+                html.P("""The Sunburst Plot efficiently displays which specific articles are alleged most commonly against each State. From these plots, we can see that Article 41 is mostly commonly alleged, however, the second most common allegation differs across each State. This information can provide a useful method to assess where a State is specifically failing in fulfilling their human rights obligations. """),
+                html.P("""The Number of Allegations by Country determined by Judge provides a quick method to assess the most prolific judges. This can prove useful to gain an insight into which judges deliver the most amount of judgements consequently to understand the most important judges in the country without conducting an primary research. """),
+            ], style={'height': '100%', 'width':'80%', 
             'background-color':'rgb(83, 130, 241)', 
             'margin': 'auto', 'font-family': 
             'Sans-serif', 'padding': '20px', 
